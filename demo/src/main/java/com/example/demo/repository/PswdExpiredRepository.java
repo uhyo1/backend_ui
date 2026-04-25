46fcecefc74160c8e2e421459c1bb98922a0db6d
uhyo <asw7@hotmail.co.jp>
2026-04-26 01:12:07 +0900

    bugFix：構成を全体的に変更

--- demo/src/main/java/com/example/demo/repository/PswdExpiredRepository.java
+++
@@ -1,7 +0,0 @@
-package com.example.demo.repository;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-import com.example.demo.entity.PswdExpired;
-
-public interface PswdExpiredRepository extends JpaRepository<PswdExpired, Integer> {
-}
--- demo/src/types/Expired.ts
+++
@@ -1,8 +0,0 @@
-export interface ExpiredItem {
-  pswdExpiredId: number
-  accessInfo: string
-  userId: string
-  password: string
-  createTime: string
-  updatedTime: string
-}
